f3349d77937379c6147fff7aaae528d852cbf487
Jolanda <jolanda@nava.pro>
2022-08-17 17:06:42 +0200

    test favicon

---
+++ build/precache-manifest.16d33263eb4903f5b138b0425caa24f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9e60c3e7e9e48e9852548c564c8faa",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "776d5def62b3615cbf0f2bf8483d30bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "591ad56bf31d42dfc7986be35f3d06b6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0ad3f85a3d10e6dd7e9d",
+    "url": "/static/css/main.43c6e6ee.chunk.css"
+  },
+  {
+    "revision": "0ad3f85a3d10e6dd7e9d",
+    "url": "/static/js/main.0ad3f85a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/static/js/vendors~main.0b49ec49.chunk.js"
+  },
+  {
+    "revision": "1ee3df488c48f3e9f92a3e961449c621",
+    "url": "/static/media/palatino-font.7a318dd7.ttf"
+  }
+]);
