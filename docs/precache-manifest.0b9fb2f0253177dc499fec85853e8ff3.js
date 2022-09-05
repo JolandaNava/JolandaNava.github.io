08365197a05019a4b6aec1d5649364ce269a8d6b
Jolanda <jolanda@nava.pro>
2022-09-05 20:15:36 +0200

    test-pages 1

---
+++ docs/precache-manifest.0b9fb2f0253177dc499fec85853e8ff3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9e60c3e7e9e48e9852548c564c8faa",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "86b2624e22ed26244980606da51804f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "591ad56bf31d42dfc7986be35f3d06b6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "8a195ec5023b3dc9ee21",
+    "url": "/static/css/main.5306bb28.chunk.css"
+  },
+  {
+    "revision": "8a195ec5023b3dc9ee21",
+    "url": "/static/js/main.8a195ec5.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/static/js/vendors~main.0b49ec49.chunk.js"
+  },
+  {
+    "revision": "8d9b50a27a9b160de30335566694903a",
+    "url": "/static/media/jost-variable.6de07354.ttf"
+  }
+]);
